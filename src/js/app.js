1757b95b27541f6576b950519e09a867a7ea5c22
b-ritter <ben@benjaminritter.com>
2016-03-06 11:00:19 -0800

    Handle super-simple anonymous user authentication

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,17 @@
 var ref = new Firebase("https://br-health-tracker.firebaseio.com");
-ref.authAnonymously(function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-  }
-});
+
+
+var userExists = ref.getAuth();
+
+if(!userExists){
+  ref.authAnonymously(function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      ref.child("users").child(authData.uid).set({
+        provider: authData.provider
+      });
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+}

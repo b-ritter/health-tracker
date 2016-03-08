8f37ce4790d42cd5fff2408af259af40345efcc3
b-ritter <ben@benjaminritter.com>
2016-03-08 09:41:03 -0800

    Update a user from Backbone. Need to make a user collection

--- src/js/views/healthtracker.js
+++ src/js/views/healthtracker.js
@@ -1,14 +1,17 @@
-var TrackerApp = Backbone.View.extend({
+var app = app || {};
+
+app.HealthTrackerView = Backbone.View.extend({
   // The main app view
   el: $('.tracker'),
   initialize: function(){
-
+    this.renderLogin();
   },
   render: function(){
 
   },
   renderLogin: function(){
-
+    var login = new app.LoginView();
+    this.$el.append(login.render().el);
   },
   renderSearch: function(){
     //TODO: Create nutrition search
--- src/js/views/login.js
+++ src/js/views/login.js
@@ -1,6 +1,8 @@
-var Login = Backbone.View.extend({
-  el: '',
-  model: User,
+var app = app || {};
+
+app.LoginView = Backbone.View.extend({
+  tag: 'div',
+  loginTemplate: _.template($('.login-template').html()),
   STATUS: {
     newLogin: 'NEWLOGIN',
     success: 'SUCCESS',
@@ -10,27 +12,56 @@ var Login = Backbone.View.extend({
     }
   },
   state: null,
+  events: {
+    'submit form': 'addUser'
+  },
+  userRef: null,
   initialize: function(){
     // Sets the state of the login
-    var userRef = new Firebase(this.model.url),
+
     // Returns null if user is not authenticated
-    isNewUser = userRef.getAuth();
-    if(isNewUser === null){
-      userRef.authAnonymously(function(error, authData) {
-        if (error) {
-          console.log("Login Failed!", error);
-        } else {
-          userRef.child("users").child(authData.uid).set({
-            provider: authData.provider
-          });
-          console.log("Authenticated successfully with payload:", authData);
-        }
-      });
+    var firebaseUrl = 'https://br-health-tracker.firebaseio.com';
+    this.userRef = new Firebase(firebaseUrl);
+    var userAuth = this.userRef.getAuth();
+
+    console.log(userAuth);
+
+    this.userRef.unauth();
+
+    if(userAuth === null){
+      this.model = new app.User();
     }else {
-      this.state = this.STATUS.success;
+      this.model = new app.User({
+        id: userAuth.uid,
+        username: userAuth.username
+      });
     }
+    this.render();
   },
   render: function(){
-
+    this.$el.html(this.loginTemplate());
+    return this;
+  },
+  addUser: function(){
+    var self = this;
+    var username = $('#userName', this.$el).val();
+    this.userRef.authAnonymously(function(error, authData) {
+      if (error) {
+        // Do I check for errors here or in the validation rules for the
+        // database?
+        console.log("Login Failed!", error);
+      } else {
+        // self.userRef.child("users").child(authData.uid).set({
+        //   provider: authData.provider,
+        //   username: username
+        // });
+        self.model.set({
+          id: authData.uid,
+          provider: authData.provider,
+          username: username
+        });
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
   }
 });

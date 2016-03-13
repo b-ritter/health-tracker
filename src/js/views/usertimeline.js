0e7b58e05170e7c7f04e4f36860e2cd79b8e3b6c
b-ritter <ben@benjaminritter.com>
2016-03-13 15:26:49 -0700

    Build out menu with bootstrap

--- src/js/views/usertimeline.js
+++ src/js/views/usertimeline.js
@@ -8,9 +8,8 @@ app.UserTimelineView = Backbone.View.extend({
     //
   },
   render: function(userAttributes){
-    this.$el.append(this.menu.render().el);
     this.$el.append(this.userTemplate({ username: userAttributes.username }));
-
+    this.$el.append(this.menu.render().el);
     return this;
   }
 });

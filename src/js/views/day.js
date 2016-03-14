4e0423d57c29dc20eb825de0682d917dce62dd33
b-ritter <ben@benjaminritter.com>
2016-03-14 09:40:09 -0700

    Adjust grid of the add day button

--- src/js/views/day.js
+++ src/js/views/day.js
@@ -1,12 +1,19 @@
 var app = app || {};
 
 app.DayView = Backbone.View.extend({
+  el: '.ht-timeline-container',
   day: {day: 'April 1, 2016'},
   dayTemplate: _.template($('.day-template').html()),
+  events: {
+    'click .add-day': 'addDay'
+  },
   initialize: function(){
     // Get latest day
   },
   render: function(){
     return this.dayTemplate( this.day );
+  },
+  addDay: function(){
+    console.log('add a day');
   }
 });

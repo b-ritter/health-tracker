1a27d119b12436ff30d114c554d6d7fcc92cb970
b-ritter <ben@benjaminritter.com>
2016-04-04 09:24:46 -0700

    Add files for constructing a view of each week

--- src/js/views/weekly.js
+++ src/js/views/weekly.js
@@ -12,28 +12,24 @@ app.WeeklyView = Backbone.View.extend({
     this.listenTo(this.parent.daysCollection, 'sync', this.render);
   },
   render: function(){
-  	// Divide the data into weekly chunks
-  	// Iterate through the list, drawing a chart for each
-  	// Chart drawing should be a function that takes an array of calories
 
-  	// Separate dates out into years
-  	// Group dates by week of year
+  	// First separate dates out into years
+
+  	var weeks_in_year = {};
 
   	var days_in_year = this.parent.daysCollection.groupBy(function(day){
   		return day.id.substr(0,4);
   	});
 
-  	// console.log(days_in_year);
-
+  	// Then group dates by week of year
   	_.each(days_in_year, function(year, index){
-  		// console.log(year, '  ', index);
-  		// console.log(days_in_year[index]);
-  		days_in_year[index] = _.groupBy(year, function(day){
+  		weeks_in_year[index] = _.groupBy(year, function(day){
 	  		return moment(day.id,'YYYY-MM-DD').week();
 	  	});
   	});
 
-  	console.log(days_in_year);
+  	// Each week in the year will have its own chart
+  	console.log(weeks_in_year);
 
     return this.weeklyTemplate({ num_days: this.parent.daysCollection.length});
   }
